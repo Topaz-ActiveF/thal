47a027bba3d4480da84b022622cd234df0a06388
Topaz-ActiveF <55323124+Topaz-ActiveF@users.noreply.github.com>
2019-09-15 21:39:48 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# start script for Thal dockerize
+
+# Enter credentials
+echo "Enter a GitHub user email"
+read username
+echo "Enter a GitHub password"
+read -s password
+
+echo "module.exports = {
+    username: '$username',
+    password: '$password'
+}" > creds.js
+
+docker-compose up
