3843406f4398e47ac3983d1bf3b0cd7f62c185b0
Topaz-ActiveF <55323124+Topaz-ActiveF@users.noreply.github.com>
2019-09-16 14:25:48 +0300

    Update index.js
    Add args required for Docker version of Puppeteer

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ async function run() {
 
   const browser = await puppeteer.launch({
     //headless: false
-	args: ['--no-sandbox']
-
+	args: [
+		// Required for Docker version of Puppeteer
+      		'--no-sandbox',
+      		'--disable-setuid-sandbox',
+      		// This will write shared memory files into /tmp instead of /dev/shm,
+      		// because Docker’s default for /dev/shm is 64MB
+      		'--disable-dev-shm-usage'
+	]
   });
 
   const page = await browser.newPage();
